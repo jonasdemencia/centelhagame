cbf0f0f157e852409a7908ebb2d84b42845f94e4
jonasdemencia <fabiorainersilva@gmail.com>
2025-03-18 00:06:00 -0300

    Update character-creation.js

--- character-creation.js
+++ character-creation.js
@@ -2,8 +2,6 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebas
 import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
 
-const auth = getAuth(); // Inicializa antes do uso
-
 const firebaseConfig = {
     apiKey: "AIzaSyC0XfvjonW2gd1eGAZX7NBYfPGMwI2siJw",
     authDomain: "centelhagame-9d511.firebaseapp.com",
