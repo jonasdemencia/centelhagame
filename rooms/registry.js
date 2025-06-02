bb892c324195a05234b3cbaac5a54753bfe292da
jonasdemencia <fabiorainersilva@gmail.com>
2025-06-01 22:07:42 -0300

    Update registry.js

--- rooms/registry.js
+++ rooms/registry.js
@@ -1,9 +1,13 @@
 import { Room1Behavior } from './behaviors/room1.js';
 import { Room2Behavior } from './behaviors/room2.js';
+import { Room2Behavior } from './behaviors/room6.js';
+
 
 export const RoomBehaviors = {
     'room-1': Room1Behavior,
-    'room-2': Room2Behavior
+    'room-2': Room2Behavior,
+    'room-6': Room2Behavior
+
 };
 
 export function getRoomBehavior(roomId) {
