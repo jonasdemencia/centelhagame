c7a0fb5a47f5c2b5f9fc46421744767138f866f7
jonasdemencia <fabiorainersilva@gmail.com>
2025-08-04 14:58:57 -0300

    Update monstros.js

--- monstros.js
+++ monstros.js
@@ -9,8 +9,7 @@ export const monsterData = {
     descricao: "Um lobo selvagem com presas afiadas.",
     habilidade: 5,
     couraça: 13,
-    pontosDeEnergia: 15,
-    pontosDeEnergiaMax: 15,
+    energiadados: "3d8",
     experiencia: 50,
     dano: "1D10", // Mantido para compatibilidade
     ataques: [
@@ -409,4 +408,5 @@ export const monsterData = {
 export function getMonsterById(monsterId) {
     return monsterData[monsterId] || null;
 }
+
 
