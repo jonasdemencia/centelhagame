d18c9de882eb78bfc1526498b4e42b436b5a6882
jonasdemencia <fabiorainersilva@gmail.com>
2025-10-07 12:29:21 -0300

    Update inventario.js

--- inventario.js
+++ inventario.js
@@ -170,7 +170,8 @@ const extraItems = [
 
 // === UTILITÁRIOS ===
 { id: "corda", content: "Corda", uuid: "extra-corda", description: "Corda resistente para escaladas.", image: "https://raw.githubusercontent.com/jonasdemencia/CentelhaGame/main/images/items/corda.png", thumbnailImage: "https://raw.githubusercontent.com/jonasdemencia/CentelhaGame/main/images/items/thucorda.png" },
-{ id: "esqueiro", content: "Esqueiro", uuid: "extra-esqueiro", description: "Produz chama contínua.", image: "https://raw.githubusercontent.com/jonasdemencia/CentelhaGame/main/images/items/esqueiro.png", thumbnailImage: "https://raw.githubusercontent.com/jonasdemencia/CentelhaGame/main/images/items/thuesqueiro.png" }
+{ id: "esqueiro", content: "Esqueiro", uuid: "extra-esqueiro", description: "Produz chama contínua.", image: "https://raw.githubusercontent.com/jonasdemencia/CentelhaGame/main/images/items/esqueiro.png", thumbnailImage: "https://raw.githubusercontent.com/jonasdemencia/CentelhaGame/main/images/items/thuesqueiro.png" },
+{ id: "cartaodeacesso1", content: "Cartao de Acesso 1", uuid: "extra-cartaodeacesso1", description: "Permite a entrada a área restrita.", image: "https://raw.githubusercontent.com/jonasdemencia/CentelhaGame/main/images/items/cartaodeacesso1.png", thumbnailImage: "https://raw.githubusercontent.com/jonasdemencia/CentelhaGame/main/images/items/thucartaodeacesso1.png" }
 
 ];
 
