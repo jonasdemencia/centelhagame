565cd3e536be97016bddd848d87b33d63f11147b
jonasdemencia <fabiorainersilva@gmail.com>
2025-10-15 14:39:54 -0300

    Update inventario.js

--- inventario.js
+++ inventario.js
@@ -757,7 +757,7 @@ document.addEventListener('click', function(event) {
 // Elementos que NÃO devem desselecionar o item
 
 // CÓDIGO CORRIGIDO
-const keepSelection = event.target.closest('.item, .slot, #useBtn, #carregar-municao-btn, #discard-slot, .dice-item, .dice-slot, .item-actions-window, .item-coletavel, #btn-sim-inv, #btn-nao-inv, .preview-info-window');
+const keepSelection = event.target.closest('.item, .slot, #useBtn, #carregar-municao-btn, #discard-slot, .dice-item, .dice-slot, .item-actions-window, .item-coletavel, .preview-image-window, .preview-image-container');
     
 if (!keepSelection && selectedItem) {
     clearHighlights();
